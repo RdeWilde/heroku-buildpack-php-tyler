b0a9eb182cd97ab427707eb4bf35b9dfd6307a58
R. de Wilde <email@robertdewilde.nl>
2014-02-15 20:43:30 +0100

    Update set-env.sh

--- support/set-env.sh
+++ support/set-env.sh
@@ -3,15 +3,15 @@
 export S3_BUCKET="heroku-buildpack-php-tyler"
 
 export LIBMCRYPT_VERSION="2.5.8"
-export LIFREETYPE_VERSION="2.4.12"
-export PHP_VERSION="5.4.17"
-export APC_VERSION="3.1.10"
-export PHPREDIS_VERSION="2.2.2"
-export LIBMEMCACHED_VERSION="1.0.7"
-export MEMCACHED_VERSION="2.0.1"
-export NEWRELIC_VERSION="2.9.5.78"
+export LIFREETYPE_VERSION="2.5.2"
+export PHP_VERSION="5.5.9"
+export APC_VERSION="3.1.13"
+export PHPREDIS_VERSION="2.2.4"
+export LIBMEMCACHED_VERSION="1.0.18"
+export MEMCACHED_VERSION="2.1.0"
+export NEWRELIC_VERSION="3.7.1.180"
 
-export NGINX_VERSION="1.4.1"
+export NGINX_VERSION="1.5.10"
 
 export EC2_PRIVATE_KEY=~/.ec2/pk.pem
 export EC2_CERT=~/.ec2/cert.pem
